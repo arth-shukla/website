afc34cae4ca5b5421a71bb3f004a0cb7a3817c26
Arth Shukla <44527268+arth-shukla@users.noreply.github.com>
2022-10-20 01:12:41 -0700

    update homepage text

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -154,7 +154,7 @@ function ProjectsInfoSection() {
 					lg={12}
 				>
 					<h1>I like making stuff</h1>
-					<p>Right now I'm very interested in web dev and AI/ML. Most of my experience with web dev is in front-end, while I've been trying to learn different things in AI.</p>
+					<p>Right now I'm very interested in web dev and AI/ML. Most of my experience with web dev is in front-end, and I've been learning NLP and the math behind optimization on the AI side.</p>
 					<p>Here are some projects I've worked on that I had fun building.</p>
 				</Grid>
 				<Grid
